58c3a02b5026565ade0b9925d71e98faf6893aa1
minhphuong2001 <minhphuongdo2001@gmail.com>
2022-07-12 22:16:54 +0700

    change message

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -10,30 +10,37 @@ module.exports = {
     return res.status(statusCodeEnum.OK).json(new ResponseBuilder({}));
   }),
 
-  // testtttt
   //@route [POST] /register
   register: asyncHandle(async (req, res, next) => {
     const { username, email, password, displayname } = req.body;
 
     if (!(username && email && password && displayname)) {
-      return res.status(statusCodeEnum.BAD_REQUEST).json({
-        message: 'Missing information'
-      })
+      return res.status(statusCodeEnum.BAD_REQUEST).json(
+        new ResponseBuilder()
+          .withMessage("Missing information")
+          .withCode(statusCodeEnum.BAD_REQUEST)
+          .build()
+        );
     }
 
     const checkEmail = await User.findOne({ email });
     if (checkEmail) {
-      return res.status(statusCodeEnum.BAD_REQUEST).json({
-        message: 'Email already exists'
-      })
+      return res.status(statusCodeEnum.BAD_REQUEST).json(
+        new ResponseBuilder()
+          .withMessage("Email already exists")
+          .withCode(statusCodeEnum.BAD_REQUEST)
+          .build()
+        );
     }
 
     const checkUsername = await User.findOne({ username });
     if (checkUsername) {
-      return res.status(statusCodeEnum.BAD_REQUEST).json({
-        success: false,
-        message: 'Username already exists'
-      })
+      return res.status(statusCodeEnum.BAD_REQUEST).json(
+        new ResponseBuilder()
+          .withMessage("Username already exists")
+          .withCode(statusCodeEnum.BAD_REQUEST)
+          .build()
+        );
     }
 
     const user = await User.create({
@@ -46,14 +53,15 @@ module.exports = {
     const accessToken = user.signAccessToken();
     const refreshToken = await user.signRefreshToken();
 
-    res.status(statusCodeEnum.CREATED).json({
-      message: 'Your account has been registered successfully!',
-      data: {
+    res.status(statusCodeEnum.OK).json(
+      new ResponseBuilder({
         user,
         accessToken,
         refreshToken
-      }
-    })
+      })
+        .withMessage("Your account has been registered successfully!")
+        .build()
+      );
   }),
 
   //@route [POST] /login
@@ -61,45 +69,54 @@ module.exports = {
     const { username, password } = req.body;
 
     if (!username || !password) {
-      return res.status(statusCodeEnum.BAD_REQUEST).json({
-        message: 'Missing information'
-      })
+      return res.status(statusCodeEnum.BAD_REQUEST).json(
+        new ResponseBuilder()
+          .withMessage("Missing information")
+          .withCode(statusCodeEnum.BAD_REQUEST)
+          .build()
+      );
     }
 
     const user = await User.findOne({ username }).select("password");
     if (!user) {
-      return res.status(statusCodeEnum.BAD_REQUEST).json({
-        message: 'Username or password is incorrect'
-      })
+      return res.status(statusCodeEnum.BAD_REQUEST).json(
+        new ResponseBuilder()
+          .withMessage("Username or password is incorrect")
+          .withCode(statusCodeEnum.BAD_REQUEST)
+          .build()
+      );
     }
 
     if (!user.matchPassword(password)) {
-      return res.status(statusCodeEnum.BAD_REQUEST).json({
-        message: 'Password is incorrect'
-      })
+      return res.status(statusCodeEnum.BAD_REQUEST).json(
+        new ResponseBuilder()
+          .withMessage("Username or password is incorrect")
+          .withCode(statusCodeEnum.BAD_REQUEST)
+          .build()
+      );
     }
 
     const accessToken = user.signAccessToken();
     const refreshToken = await user.signRefreshToken();
 
-    res.status(statusCodeEnum.CREATED).json({
-      message: 'Login successfully!',
-      data: {
+    res.status(statusCodeEnum.OK).json(
+      new ResponseBuilder({
         accessToken,
         refreshToken
-      }
-    })
+      })
+        .withMessage("Login successfully!")
+        .build()
+      );
   }),
 
   //@route [POST] /new-token
   newToken: asyncHandle(async (req, res, next) => {
-    const token = req.user.signAccessToken();
+    const accessToken = req.user.signAccessToken();
 
-    res.status(statusCodeEnum.CREATED).json({
-      message: 'Token has been refreshed successfully!',
-      data: {
-        token
-      }
-    })
+    res.status(statusCodeEnum.OK).json(
+      new ResponseBuilder({ accessToken })
+        .withMessage("Token has been refreshed successfully!")
+        .build()
+      );
   })
 };
