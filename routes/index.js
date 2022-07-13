07a0bf072fc7459a83310dc123b894f67ccdf020
Đông <69163058+badongit@users.noreply.github.com>
2022-07-13 11:38:15 +0700

    Revert "Auth login"

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 const routes = require("../enum/router.enum");
-const errorHandle = require("../middlewares/error-handle");
 
 module.exports = (app) => {
   app.get("/", (req, res) => {
@@ -9,8 +8,6 @@ module.exports = (app) => {
   routes.forEach((route) => {
     app.use(`/api/${route.prefix}`, route.router);
   });
-
-  app.use(errorHandle);
 
   app.get("*", (req, res) => {
     return res.status(404).json({
