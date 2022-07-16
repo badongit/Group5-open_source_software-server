c4d9301cb13fe32d49e6c0e66f3b36229811be75
minhphuong2001 <minhphuongdo2001@gmail.com>
2022-07-16 17:06:18 +0700

    create conversation

--- socket/listeners/index.js
+++ socket/listeners/index.js
@@ -1,5 +1,9 @@
 const disconnect = require("./disconnect");
+const joinRoom = require("./join-room");
+const createConversation = require("./create-conversation");
 
 module.exports = {
   disconnect,
+  joinRoom,
+  createConversation,
 };
---
+++ socket/listeners/join-room.js
@@ -0,0 +1,28 @@
+const User = require("../../models/User");
+const Conversation = require("../../models/Conversation");
+const SocketEvent = require("../constants/socket-event");
+const socketMsg = require("../constants/socket-msg");
+
+module.exports = (io, socket) => async (req) => {
+  try {
+    const conversation = await Conversation.findOne({
+      _id: conversationId,
+      members: socket.currentUser._id,
+    })
+      .populate({ path: "members" })
+      .populate({ path: "admin" })
+      .populate({ path: "lastMessage" })
+      .lean();
+
+    if (!conversation) {
+      return socket.emit(SocketEvent.ERROR, {
+        message: socketMsg.NOT_FOUND.replace(":{message}", "conversation"),
+      });
+    }
+
+    socket.join(conversationId);
+    socket.emit(SocketEvent.SV_SEND_CONVERSATION, { conversation });
+  } catch (error) {
+    console.log(`Error socket: ${error.message}`);
+  }
+};
