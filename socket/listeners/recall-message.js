620fdfb9654ae470c1cae984399fa47fe5a2b22c
DONG Nguyen Ba (VTI.D1) <dong.nguyenba@vti.vn>
2022-07-27 16:16:18 +0700

    add socket recall message

---
+++ socket/listeners/recall-message.js
@@ -0,0 +1,46 @@
+const Conversation = require("../../models/Conversation");
+const Message = require("../../models/Message");
+const socketEvent = require("../constants/socket-event");
+const socketMsg = require("../constants/socket-msg");
+
+module.exports = (io, socket) => async (req) => {
+  try {
+    const message = await Message.findById(req.messageId).populate("sender");
+
+    if (!message) {
+      return socket.emit(socketEvent.ERROR, {
+        message: socketMsg.NOT_FOUND.replace(":{entity}", "message"),
+      });
+    }
+
+    if (message.sender._id !== socket.currentUser._id || message.deletedAt) {
+      return socket.emit(socketEvent.ERROR, {
+        message: socketMsg.FORBIDDEN,
+      });
+    }
+
+    await message.recall();
+
+    if (message.conversation._id) {
+      io.in(message.conversation._id.toString()).emit(
+        socketEvent.SV_SEND_MESSAGE,
+        message
+      );
+    }
+
+    if (message._id === message.conversation.lastMessage) {
+      const conversation = await Conversation.findById(
+        message.conversation._id
+      ).populate(["members", "admin", "lastMessage"]);
+
+      if (conversation) {
+        io.in(conversation._id.toString()).emit(
+          socketEvent.SV_SEND_CONVERSATION,
+          conversation
+        );
+      }
+    }
+  } catch (error) {
+    socket.emit(socketEvent.ERROR, { message: error.message });
+  }
+};
