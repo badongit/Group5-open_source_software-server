af631d982b5b775b0ab6519c6a4fcd6c51e37ccf
Nguyễn Đông <nguyenbadong01@gmail.com>
2022-08-23 21:33:34 +0700

    fix error duplicate member of conversation

--- socket/listeners/create-conversation.js
+++ socket/listeners/create-conversation.js
@@ -17,8 +17,8 @@ module.exports = (io, socket) => async (req) => {
       });
     }
 
-    members = users.map((user) => user._id);
-    members = toArrayUnique([...members, socket.currentUser._id]);
+    members = users.map((user) => user._id.toString());
+    members = toArrayUnique([...members, socket.currentUser._id.toString()]);
 
     if (members?.length < 3 || members?.length > 31) {
       return socket.emit(socketEvent.ERROR, {
